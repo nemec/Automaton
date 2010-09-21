7d290f1d39a7c3c45dbb17267ed10d166f51aa76
Dan N <djnemec@gmail.com>
2010-09-20 23:05:26 -0500

    Added code to unescape HTML special characters in the AIM bot

--- AIM.py
+++ AIM.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+sys.path.append('/home/dan/prg/py/Automaton/Automaton')
 from twisted.words.protocols import oscar
 from twisted.internet import protocol, reactor
 import Automaton.lib.settings_loader as settings_loader
@@ -8,6 +9,36 @@ import Automaton.lib.settings_loader as settings_loader
 import Automaton.lib.logger as logger
 
 import Automaton.lib.ClientWrapper as ClientWrapper
+
+## unescape
+# Removes HTML or XML character references and entities from a text string.
+#
+# @param text The HTML (or XML) source text.
+# @return The plain text, as a Unicode string, if necessary.
+# Provided by Fredrik Lundh at http://effbot.org/zone/re-sub.htm#unescape-html
+import re, htmlentitydefs
+
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
+
 
 op = {'HOST':'login.oscar.aol.com',
       'PORT':'5190'
@@ -50,8 +81,8 @@ class B(oscar.BOSConnection):
           returned = ''
           args=''
           if ix > -1:
-            args = body[ix+1:]
-            body = body[0:ix]
+            args = unescape(body[ix+1:])
+            body = unescape(body[0:ix])
 
           if self.client.isScript(body):
             self.client.registerScript(body)
