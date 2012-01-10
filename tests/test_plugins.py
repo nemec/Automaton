1b41298f5fab971e76b7e4c0bbe8a3776e6fb4a8
Nemec <djnemec@gmail.com>
2012-01-09 22:13:04 -0600

    Began updating plugins with unit tests.\nPlugins can now add a subclass of plugin.RegistrationTestCase to have its unit tests be automatically tested. The superclass has a couple of useful convenience functions for testing common processes.

---
+++ tests/test_plugins.py
@@ -0,0 +1,12 @@
+try:
+  import unittest2 as unittest
+except ImportError:
+  import unittest
+
+loader = unittest.TestLoader()
+test_suite = loader.discover(
+  "automaton.plugins", pattern="*.py", top_level_dir='..')
+
+if __name__ == "__main__":
+  runner = unittest.runner.TextTestRunner()
+  runner.run(test_suite)
