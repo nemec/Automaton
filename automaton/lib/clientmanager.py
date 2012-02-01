5fe92698626eaddd37d24219d53477c9bfd3e829
Daniel Nemec <djnemec@sun.(none)>
2012-02-01 14:04:13 -0600

    Added new features to ClientManager

--- automaton/lib/clientmanager.py
+++ automaton/lib/clientmanager.py
@@ -2,15 +2,22 @@ import time
 from threading import Lock
 from collections import deque
 
-
 class Client(object):  # pylint: disable-msg=R0903
   """Represents a connected client."""
-  def __init__(self, max_history):
+  def __init__(self, max_history, name, uid, availablility):
     self.history = deque(maxlen=max_history)
+	self.name = name
+	self.uid = uid
+	self.availability = availability
     self.current_conversation = None
     self.last_contact = time.time()
     self.plugins = set()
     self.lock = Lock()
+
+  def __str__(self):
+    return "<Client {0}, {1}, last contacted: {2}".format(
+	  self.name, self.availability,
+	  time.strftime("%d/%m/%Y %I:%M:%S%p"))
 
 
 class ClientManager(object):  # pylint: disable-msg=R0903
@@ -19,6 +26,19 @@ class ClientManager(object):  # pylint: disable-msg=R0903
     self.max_history = max_history
     self.registered_clients = {}
 
-  def add_client(self, ident):
+  def add_client(self, ident, name=None, availability="transient"):
     """Add a client to the list of registered clients."""
-    self.registered_clients[ident] = Client(self.max_history)
+    self.registered_clients[ident] = Client(max_history=self.max_history,
+	  name=(name or ident), uid=ident, availability=availability)
+
+  def get_persistent_clients(self):
+    return [x for x in self.registered_clients.values()
+	          if x.availability == "persistent"]
+
+  def get_transient_clients(self):
+    return [x for x in self.registered_clients.values()
+	          if x.availability == "transient"]
+
+  def get_active_clients(self):
+    return sorted(self.registered_clients.values(),
+		key=lambda client: client.last_contact, reversed=True)
